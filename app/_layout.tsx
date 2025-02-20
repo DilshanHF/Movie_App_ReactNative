0a2b2c6bf1365bec8563402e2d0e47ff2d664941
DILSHAN <dilshanfonseka76@gmail.com>
2025-02-20 13:29:59 +0530

    Home tab done

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -5,7 +5,6 @@ export default function RootLayout () {
     return (
         <Stack screenOptions={{ headerShown: false }}>
             <Stack.Screen name="index"  /> {/* This will render the WelcomeScreen */}
-            <Stack.Screen name="search" /> {/* Dynamic route for MovieScreen */}
             <Stack.Screen name="home" /> {/* Dynamic route for PersonScreen */}
             {/* Search screen can be accessed directly via /search */}
         </Stack>
